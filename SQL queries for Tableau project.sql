bcf0314037f40f5b7655ab05af509b1836cf238b
Osiomogie1 <105092887+Osiomogie1@users.noreply.github.com>
2022-12-28 18:15:03 -0700

    Add files via upload
    This project was used to visualise in Tableau.

---
+++ SQL queries for Tableau project.sql
@@ -0,0 +1,41 @@
+
+
+--Showing total of cases, death and percentage
+--1
+Select SUM(new_cases) as Total_cases , SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+From CovidDeaths
+--Where location = 'Canada'
+Where continent is not null
+--Group by date
+Order by 1, 2
+--
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
